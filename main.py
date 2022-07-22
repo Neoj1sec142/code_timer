bc2620ff4b30de0102bcf97f2ac0322931b7b1ce
Mark Harmon <neothej1sec@gmail.com>
2022-07-22 15:50:46 -0500

    finished up reset button functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 from tkinter import *
 from tkinter import messagebox as mb
 import time
+import os
 import sys
 win = Tk()
 win.geometry('750x300')
@@ -16,12 +18,22 @@ mins.set('00')
 hrs= StringVar()
 Entry(win, textvariable = hrs, width =2, font = 'Serif 14').place(x=142, y=120)
 hrs.set('00')
-times = 0
+timer_btn = False
+def stop_btn():
+      dt = mb.askyesno("Warning,", "Are you sure you wish to stop the clock?")
+      if dt is True:
+         os.execv(__file__, sys.argv)
+
+
 def thetimer():
-    times = int(hrs.get())*3600+ int(mins.get())*60 + int(sec.get())
-    while times > -1:
+   times = int(hrs.get())*3600+ int(mins.get())*60 + int(sec.get())
+   # while times > -1:
+   timer_btn = True
+   # user = ''
+   while timer_btn == True:
       minute,second = (times // 60 , times % 60)
       hour =0
+      # user = input()
       if minute > 60:
          hour , minute = (minute // 60 , minute % 60)
       sec.set(second)
@@ -53,5 +65,6 @@ def exit():
 
 Label(win, font =('Serif bold',22), text = 'Start the Timer',bg='white').place(x=105,y=70)
 Button(win, text='START', bd ='2', bg = 'IndianRed1',font =('Serif',10), command = thetimer).place(x=167, y=165)
+Button(win, text='STOP', bd ='2', bg = 'IndianRed1',font =('Serif',10), command = stop_btn).place(x=167, y=205)
 Button(win, text='Exit', bd ='2', bg = 'IndianRed1',font =('Serif',10), command = exit).place(x=100, y=165)
 win.mainloop()
